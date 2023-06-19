bea0c9a302e1c1f90cba7a09d1594497e440b125
hernancarreno01 <radioportatil@gmail.com>
2023-06-18 22:28:13 -0300

    Ejemplos Funciones

---
+++ app.js
@@ -0,0 +1,106 @@
+const numeros = [1,14,54,84,10, 1,46,385,80]
+
+//console.log(numeros[numeros.length])
+/*
+let prueba = numeros.push(45)
+
+console.log(numeros)
+console.log(prueba)
+
+numeros.push(17,47,37,27,87,97,67,77)
+
+console.log(numeros)
+*/
+
+/*
+let ultimoElemento = numeros.pop()
+
+console.log(ultimoElemento)
+console.log(numeros)
+*/
+
+/*
+let primerElemento = numeros.shift()
+console.log(primerElemento)
+console.log(numeros)
+*/
+/*
+let nuevaLongitud = numeros.unshift(89)
+console.log(nuevaLongitud)
+console.log(numeros)
+*/
+/*
+let separadoPorComas = numeros.join()
+let separadoPorBarras = numeros.join(" / ")
+let separadoPorEspacios = numeros.join(" ")
+let separadoPorNada = numeros.join("")
+
+console.log(separadoPorComas)
+console.log(separadoPorBarras)
+console.log(separadoPorEspacios)
+console.log(separadoPorNada)
+console.log(numeros)
+*/
+/*
+let indice = numeros.indexOf(54)
+let indice = numeros.indexOf(9999999)
+console.log(indice)
+*/
+/*
+let ultimoIndice = numeros.lastIndexOf(1)
+console.log(ultimoIndice)
+*/
+/*
+let existe = numeros.includes(1)
+let existe = numeros.includes(99999999)
+console.log(existe)
+*/
+
+
+let texto = "habia una vez"
+
+//console.log(texto[1])
+
+
+//console.log(texto.length)
+
+//let indice = texto.indexOf("a")
+//let indice = texto.indexOf("una")
+//let indice = texto.indexOf("había un")
+//let indice = texto.indexOf("habia un")
+//console.log(indice)
+
+//texto.push("hola") // NOS DA ERROR
+/*
+let primerPrueba = texto.slice(1,6)
+let segundaPrueba = texto.slice(6)
+let tercerPrueba = texto.slice(-7)
+
+console.log(primerPrueba)
+console.log(segundaPrueba)
+console.log(tercerPrueba)
+*/
+/*
+let nuevoString = texto.replace("i", "í")
+console.log(nuevoString)
+*/
+
+/*
+let nuevoTexto = "            Hola          como estas          "
+//let nuevoTexto = "            Hola como estas          "
+let sinEspacios = nuevoTexto.trim()
+console.log(sinEspacios)
+*/
+
+let nuevoString = "Hola, como estas, me gustaria, comer, helado,!"
+
+//let arrayDeString = nuevoString.split()
+//let arrayDeString = nuevoString.split("")
+//let arrayDeString = nuevoString.split(" ")
+/*
+let arrayDeString = nuevoString.split(",")
+console.log(arrayDeString)
+
+let stringArreglado = arrayDeString.join("")
+console.log(stringArreglado)
+*/
